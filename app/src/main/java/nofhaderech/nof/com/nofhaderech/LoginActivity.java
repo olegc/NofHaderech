123b7cfeb7b7958aad2bd9a545a0f479168644de
Oleg <olegchernoguz@gmail.com>
2017-06-19 11:06:02 +0300

    Translating to Hebrew

--- app/src/main/java/nofhaderech/nof/com/nofhaderech/LoginActivity.java
+++ app/src/main/java/nofhaderech/nof/com/nofhaderech/LoginActivity.java
@@ -94,6 +94,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
     }
 
     private void populateAutoComplete() {
