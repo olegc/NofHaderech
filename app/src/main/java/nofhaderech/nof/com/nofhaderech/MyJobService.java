ddf574e9bc24f394cfa5f338e03a913e9fb8380f
OrenShm <OrenSmo@gmail.com>
2017-06-20 14:19:44 +0300

    Merge branch 'master' of https://github.com/olegc/NofHaderech
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/nofhaderech/nof/com/nofhaderech/MyJobService.java
+++ app/src/main/java/nofhaderech/nof/com/nofhaderech/MyJobService.java
@@ -1,8 +1,29 @@
 package nofhaderech.nof.com.nofhaderech;
 
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
 /**
  * Created by USER on 6/20/2017.
  */
 
-public class MyJobService {
+
+public class MyJobService extends JobService {
+
+    private static final String TAG = "MyJobService";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "Performing long running task in scheduled job");
+        // TODO(developer): add long running task here.
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+
 }
