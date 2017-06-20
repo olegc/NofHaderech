ed276f5fd072c2258afdae4b0670cc0f83654153
OrenShm <OrenSmo@gmail.com>
2017-06-20 18:08:17 +0300

    Add notification popup

--- app/src/main/java/nofhaderech/nof/com/nofhaderech/MyFirebaseMessagingService.java
+++ app/src/main/java/nofhaderech/nof/com/nofhaderech/MyFirebaseMessagingService.java
@@ -70,6 +70,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
+
+        Intent intent = new Intent(MyFirebaseMessagingService.this, ApproveRideActivity.class);
+        startActivity(intent);
+
+
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
