cd75b0cb4ef001f0466eae0985162e608e249756
Oleg <olegchernoguz@gmail.com>
2017-06-20 14:47:18 +0300

    Adding approved ride page and colors

---
+++ app/src/main/java/nofhaderech/nof/com/nofhaderech/RideApprovedActivity.java
@@ -0,0 +1,13 @@
+package nofhaderech.nof.com.nofhaderech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RideApprovedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_approved);
+    }
+}
